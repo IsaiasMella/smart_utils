30c84c00fdb898075fbed0dea51d2499d1b3dc7e
IsaiasDesky <isaias@desky.la>
2024-07-16 22:46:37 -0300

    tests:added test for objects functions

--- src/objects/merge.ts
+++ src/objects/merge.ts
@@ -5,7 +5,8 @@ interface Args {
 }
 
 export const merge = <T>(objArray: Args[]): T => {
-  if (!isArray(objArray)) throw new Error('The argument is not an array');
+  // Stryker disable next-line all
+  if (!isArray(objArray)) {throw new Error('The argument is not an array');}
 
   return objArray.reduce((prev, current) => {
     return { ...prev, ...current.obj };
